ab86e5e7359b90982f5e6bebe187f707f9d51dd5
Fady Selim <fadyselim2005@gmail.com>
2025-12-18 06:41:00 +0200

    update

--- PatronDashboard.java
+++ PatronDashboard.java
@@ -1,5 +1,5 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class PatronDashboard extends JFrame {
 
--- PatronDashboardGUI.java
+++ PatronDashboardGUI.java
@@ -9,7 +9,7 @@ public class PatronDashboardGUI extends JFrame {
         this.patron = patron;
 
         setTitle("Patron Dashboard");
-        setSize(300, 220);
+        setSize(300, 320);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -17,38 +17,34 @@ public class PatronDashboardGUI extends JFrame {
         welcome.setBounds(30, 20, 220, 25);
         add(welcome);
 
+        // Buttons (same width, different Y positions)
         JButton search = new JButton("Search Book");
         search.setBounds(70, 60, 160, 30);
         add(search);
 
-
-//diminsoins for buttons are same, need to change
         JButton reserveBook = new JButton("Reserve Book");
-        search.setBounds(70, 60, 160, 30);
-        add(search);
+        reserveBook.setBounds(70, 100, 160, 30);
+        add(reserveBook);
 
         JButton returnBook = new JButton("Return Book");
-        search.setBounds(70, 60, 160, 30);
-        add(search);
+        returnBook.setBounds(70, 140, 160, 30);
+        add(returnBook);
 
-        JButton viewChecoutHistory = new JButton("View Checout History");
-        search.setBounds(70, 60, 160, 30);
-        add(search);
-// ------------------------------------------------------------------------------
-
+        JButton viewCheckoutHistory = new JButton("View Checkout History");
+        viewCheckoutHistory.setBounds(50, 180, 200, 30);
+        add(viewCheckoutHistory);
 
         JButton update = new JButton("Manage Account");
-        update.setBounds(70, 100, 160, 30);
+        update.setBounds(70, 220, 160, 30);
         add(update);
 
         JButton logout = new JButton("Logout");
-        logout.setBounds(70, 140, 160, 30);
+        logout.setBounds(70, 260, 160, 30);
         add(logout);
 
+        // Actions
         search.addActionListener(e -> new SearchBookGUI());
-
         update.addActionListener(e -> new ManageUserGUI(patron.getId(), patron));
-
         logout.addActionListener(e -> {
             dispose();
             new LoginGUI();
