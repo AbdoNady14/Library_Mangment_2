9d30286d1f2d3248204914b2d8ec34087444ce35
AbdoNady14 <abdonady140@gmail.com>
2025-12-18 06:49:42 +0200

    .

--- PatronDashboardGUI.java
+++ PatronDashboardGUI.java
@@ -9,7 +9,7 @@ public class PatronDashboardGUI extends JFrame {
         this.patron = patron;
 
         setTitle("Patron Dashboard");
-        setSize(300, 320);
+        setSize(300, 350);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -17,17 +17,16 @@ public class PatronDashboardGUI extends JFrame {
         welcome.setBounds(30, 20, 220, 25);
         add(welcome);
 
-        // Buttons (same width, different Y positions)
         JButton search = new JButton("Search Book");
-        search.setBounds(70, 60, 160, 30);
+        search.setBounds(50, 60, 200, 30);
         add(search);
 
         JButton reserveBook = new JButton("Reserve Book");
-        reserveBook.setBounds(70, 100, 160, 30);
+        reserveBook.setBounds(50, 100, 200, 30);
         add(reserveBook);
 
         JButton returnBook = new JButton("Return Book");
-        returnBook.setBounds(70, 140, 160, 30);
+        returnBook.setBounds(50, 140, 200, 30);
         add(returnBook);
 
         JButton viewCheckoutHistory = new JButton("View Checkout History");
@@ -35,11 +34,11 @@ public class PatronDashboardGUI extends JFrame {
         add(viewCheckoutHistory);
 
         JButton update = new JButton("Manage Account");
-        update.setBounds(70, 220, 160, 30);
+        update.setBounds(50, 220, 200, 30);
         add(update);
 
         JButton logout = new JButton("Logout");
-        logout.setBounds(70, 260, 160, 30);
+        logout.setBounds(50, 260, 200, 30);
         add(logout);
 
         // Actions
